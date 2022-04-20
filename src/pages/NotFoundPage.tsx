24703a83c98dc4b800ebe9c4b21d4e73998b5396
jsmithb117 <jsmithb117@gmail.com>
2022-04-20 09:21:38 -0700

    Adds Interstate 404 image to NotFoundPage

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ROUTES } from '../resources/routes-constants'
+import i404 from '../assets/I404.png';
 
 const NotFoundPage: React.FC = () => {
     const navigate = useNavigate()
@@ -14,9 +15,10 @@ const NotFoundPage: React.FC = () => {
 
     return (
         <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-            <h1 style={{ fontSize: '4em' }}>Oops 404!</h1>
+            <h1 className="not-found-text" style={{ fontSize: '4em' }}>Oops, we ended up on Interstate 404!</h1>
+            <img className="404-image" src={i404} alt="404, page not found" height="100" />
             <span style={{ cursor: 'pointer' }} onClick={() => redirectToHomePage()}>
-                Homepage
+                Get off the 404 and go to the homepage!
             </span>
         </div>
     )
