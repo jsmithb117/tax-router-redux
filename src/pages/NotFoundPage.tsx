2ba069c13c5ec233907868e45d156f9caa60700c
jsmithb117 <jsmithb117@gmail.com>
2022-04-20 09:25:14 -0700

    Uses MUI button instead of span with a clickHandler

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ROUTES } from '../resources/routes-constants'
 import i404 from '../assets/I404.png';
+import Button from '@mui/material/Button';
 
 const NotFoundPage: React.FC = () => {
     const navigate = useNavigate()
@@ -17,9 +18,7 @@ const NotFoundPage: React.FC = () => {
         <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
             <h1 className="not-found-text" style={{ fontSize: '4em' }}>Oops, we ended up on Interstate 404!</h1>
             <img className="404-image" src={i404} alt="404, page not found" height="100" />
-            <span style={{ cursor: 'pointer' }} onClick={() => redirectToHomePage()}>
-                Get off the 404 and go to the homepage!
-            </span>
+            <Button variant="outlined" onClick={() => redirectToHomePage()}>Get me off the 404!</Button>
         </div>
     )
 }
