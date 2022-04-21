be0997fd5d8721312768d270c867d80b1aea93f9
jsmithb117 <jsmithb117@gmail.com>
2022-04-21 10:48:54 -0700

    Adds RootComponent to main App div

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ const App: React.FC = () => {
     <Provider store={store}>
       <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
         <h1 style={{ fontSize: '4em' }}>{"Jay's Tax Estimator"}</h1>
+        <RootComponent />
       </div>
-      <RootComponent />
     </Provider>
   )
 }
