5eefb70e49a2230f9cc9c9292d938ed9840ed83f
jsmithb117 <jsmithb117@gmail.com>
2022-04-27 09:18:23 -0700

    Animates Year form

--- src/pages/Year.tsx
+++ src/pages/Year.tsx
@@ -1,9 +1,11 @@
 // External function/data imports
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from 'react-router-dom'
 
 // External Component imports
+import Fade from 'react-reveal/Fade';
+
 // Internal function/data imports
 import { RootState } from "../store/store";
 import { updateFilingYear } from "../store/slices/yearSlice";
@@ -20,22 +22,35 @@ const Year = () => {
 
   const year = useSelector((state: RootState) => state.year);
 
+  const [show, setShow] = React.useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(true);
+    }, 150);
+  }, [])
   const clickHandler = (val: string) => {
     dispatch(updateFilingYear(val));
   };
   const redirectToStatus = () => {
-    navigate(ROUTES.STATUS_ROUTE);
+    setShow(false);
+    setTimeout(() => {
+      navigate(ROUTES.STATUS_ROUTE);
+    }, 500)
   };
 
   return (
     <div className="input-year">
-      <SimpleInput
-        label={"Tax Year"}
-        value={year.filingYear}
-        options={year.options}
-        dataHandler={clickHandler}
-        nextHandler={redirectToStatus}
-      />
+      <Fade left opposite when={show}>
+
+        <SimpleInput
+          label={"Tax Year"}
+          value={year.filingYear}
+          options={year.options}
+          dataHandler={clickHandler}
+          nextHandler={redirectToStatus}
+        />
+      </Fade>
     </div>
   )
 }
