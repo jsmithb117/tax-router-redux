de36283faa79b56d4c3e33d81252815c4bbf5b28
jsmithb117 <jsmithb117@gmail.com>
2022-05-05 13:17:55 -0700

    Changes test

--- src/tests/pages/NotFoundPage.test.tsx
+++ src/tests/pages/NotFoundPage.test.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { render, screen } from '@testing-library/react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import NotFoundPage from '../../pages/NotFoundPage'
@@ -8,6 +9,6 @@ test('renders error message', () => {
             <NotFoundPage />
         </Router>
     )
-    const errorMessage = screen.getByText(/Oops 404!/i)
+    const errorMessage = screen.getByText("Oops, we ended up on Interstate 404!")
     expect(errorMessage).toBeInTheDocument()
 })
