415c294bb03e779802b6b184914c16a56d5edb71
jsmithb117 <jsmithb117@gmail.com>
2022-05-10 07:42:38 -0700

    Refactors to useLocation instead of useParams for testing compatibility

--- src/pages/IncomeSalary.tsx
+++ src/pages/IncomeSalary.tsx
@@ -1,7 +1,8 @@
 // External function/data imports
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
 // External Component imports
 import Fade from 'react-reveal/Fade';
@@ -25,8 +26,8 @@ const IncomeSalary = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { incomeIdString } = useParams();
-  const incomeId = parseInt(incomeIdString || "");
+  const incomeIdString = useLocation().pathname.split('/')[2];
+  const incomeId = parseInt(incomeIdString);
 
   const salary = useSelector(selectSalary(incomeId)) || 0;
 
